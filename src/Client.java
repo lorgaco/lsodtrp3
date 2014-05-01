429705051d809ee220e4aa1672f0cef1aa06566c
lorgaco <lorenzo.gaco@gmail.com>
2014-05-01 12:17:39 +0200

    Fixing args issues.

--- src/Client.java
+++ src/Client.java
@@ -253,10 +253,7 @@ public class Client {
         }
         else{
             try {
-                System.out.println("args length: " + args.length);
-                System.out.println("args: " + args[0]);
-                System.out.println("args: " + args[1]);
-                if(args.length>4) {
+                if(args.length>=4) {
                     if(args[1].equals("-k") || args[1].equals("-K")) {
                         Key=args[2];
                         System.out.println("Admin key: " + Key);
